aa8104d27fb8cba7405e62170492e8397aeb7daf
karanw330 <karanwadhwani2005@gmail.com>
2025-05-15 02:56:56 +0530

    second

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+import google.generativeai as genai
+import requests
+from flask import Flask, render_template, request, jsonify
+import datetime
+from secrets import *
+
+from google.auth.exceptions import DefaultCredentialsError
+
 
 app = Flask(__name__)
+
+genai.configure(api_key=googleapi_key)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 @app.route('/')
 def home():
     return render_template("dash.html")
+
+@app.route('/response', methods=['POST', 'GET'])
+def response():
+    if request.method == 'POST':
+        try:
+            message = request.form['input']
+            reply = model.generate_content(message).text
+            print(reply)
+            return jsonify(reply)
+        except DefaultCredentialsError:
+            return jsonify('sorry')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
